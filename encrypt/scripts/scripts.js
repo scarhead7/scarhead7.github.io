62f562db8fb153c085e94fc6180069a993af149d
Dallin Snell <scarhead7@users.noreply.github.com>
2019-05-31 17:47:12 -0600

    Update scripts.js
    * fixed missing parenthesis

--- encrypt/scripts/scripts.js
+++ encrypt/scripts/scripts.js
@@ -6,7 +6,7 @@
 function ENCRYPTING()
 {
   var key = "abcd1234";
-  var encrypted = CryptoJS.AES.encrypt($("#PLAINTEXT".html(), key);
+  var encrypted = CryptoJS.AES.encrypt($("#PLAINTEXT".html()), key);
 
   $("#ciphertext").html(encrypted.toString());
   
