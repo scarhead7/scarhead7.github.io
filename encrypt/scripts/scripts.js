35d9190f8020364611005e0de108ece4ade8c5b2
Dallin Snell <scarhead7@users.noreply.github.com>
2019-05-31 16:51:09 -0600

    Update scripts.js

--- encrypt/scripts/scripts.js
+++ encrypt/scripts/scripts.js
@@ -8,4 +8,4 @@ var key = "!abcd1234!";
 var encrypted = CryptoJS.AES.encrypt("Dallin", key);
 
 $("#ciphertext").html(encrypted.toString());
-$("#plaintext").html(CryptoJS.AES.decrypt(encrypted, key));
+$("#plaintext").html(CryptoJS.AES.decrypt(encrypted, key).toString(CryptoJS.enc.Utf8));
