241bf7b50036065cbe843647c1387a432e03b1e4
Dallin Snell <scarhead7@users.noreply.github.com>
2019-06-06 07:06:00 -0600

    Update scripts.js
    * added code to blank out the password once encrypting/decrypting is complete
    * added documentation

--- encrypt/scripts/scripts.js
+++ encrypt/scripts/scripts.js
@@ -5,17 +5,26 @@
 
 function ENCRYPTING()
 {
+  // get the password
   var key = ($("#PASSKEY").val());
+  // do the actual encrypting
   var encrypted = CryptoJS.AES.encrypt($("#PLAINTEXT").val(), key);
 
+  // place the encrypted string where the user can see
   $("#CIPHERTEXT").html(encrypted.toString());
-  
+  // blank out the password
+  $("#PASSKEY").val("");
 } // end of ENCRYPTING()
 
 function DECRYPTING()
 {
+  // get the password
   var key = ($("#PASSKEY").val());
+  // decrypt string
   var decrypted = CryptoJS.AES.decrypt($("#ENCRYPTED").val(), key);
 
+  // place the decrypted string where the user can see
   $("#DECRYPTED").html(decrypted.toString(CryptoJS.enc.Utf8));
+  // blank out the password
+  $("#PASSKEY").val("");
 } // end of DECRYPTING()
